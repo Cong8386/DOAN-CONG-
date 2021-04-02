0d9bca5ba560c6ff19416418fff9c0fac240e016
ngô doãn công <81790575+Cong8386@users.noreply.github.com>
2021-04-02 11:34:40 +0700

    nop bai 2

--- main.c
+++ main.c
@@ -3,9 +3,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-
-BEGIN
-DISPLAY 'HELLO WORLD'
-END
-
+int main(int argc, char *argv[]) {
+	int num1; /* first number to be read from user */
+	int num2; /*second number to be read from user */
+	
+	printf ("enter two integers, and I will tell you\n");
+	printf ("the relationships they satisfy:");
+	
+	scanf ("%d %d" , &num1, &num2); /*read two integers */
+	if(num1==num2)
+	{
+		printf ( "%d is equal to %d\n", num1, num2);
+		} /*end if*/
+		
+		
+	
+	 if( num1!= num2)
+	 {
+	printf ( "%d khong bang %d\n", num1, num2);} /*end if */
+	if (num1>num2)
+	{ printf ( "%d lon hon %d\n", num1, num2);} /*end if */
+	if (num1 < num2){
+		printf ( "%d nho hon %d\n", num1, num2);} /*end if*/
+	if (num <= num2){ printf ( "%")	
+		return 0;
+		
+	
+}
